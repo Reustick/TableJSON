5950d87e47ef61740f399f33baf97b9602e0b5f3
Rustam <rustam391@gmail.com>
2022-01-30 01:03:37 +0300

    Добавит вебпак

---
+++ public/bandle.js
@@ -0,0 +1,116 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/components/MainComponent.scss":
+/*!*******************************************!*\
+  !*** ./src/components/MainComponent.scss ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://tablejson/./src/components/MainComponent.scss?");
+
+/***/ }),
+
+/***/ "./src/styles/all.scss":
+/*!*****************************!*\
+  !*** ./src/styles/all.scss ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://tablejson/./src/styles/all.scss?");
+
+/***/ }),
+
+/***/ "./src/components/MainComponent.js":
+/*!*****************************************!*\
+  !*** ./src/components/MainComponent.js ***!
+  \*****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _MainComponent_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./MainComponent.scss */ \"./src/components/MainComponent.scss\");\n\n\n\nconst MainComponent = (text = 'Default text') => {\n    const box = document.createElement('div')\n    box.classList.add('main-component-box')\n    box.style.border = \"1px solid #CCC\"\n\n    box.textContent = text\n\n    return box\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (MainComponent);\n\n\n\n//# sourceURL=webpack://tablejson/./src/components/MainComponent.js?");
+
+/***/ }),
+
+/***/ "./src/main.js":
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _styles_all_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/all.scss */ \"./src/styles/all.scss\");\n/* harmony import */ var _components_MainComponent__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/MainComponent */ \"./src/components/MainComponent.js\");\n\n\n\nconst component = (0,_components_MainComponent__WEBPACK_IMPORTED_MODULE_1__[\"default\"])('My component text')\n\ndocument.querySelector('body').appendChild(component)\n\n\n\n//# sourceURL=webpack://tablejson/./src/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/main.js");
+/******/ 	
+/******/ })()
+;
--- src/main.js
+++ src/main.js
@@ -1,38 +1,7 @@
+import './styles/all.scss'
+import MainComponent from './components/MainComponent'
 
+const component = MainComponent('My component text')
 
-
-// fetch('./data.json')
-//   .then(response => {
-//     if (response.status === 200) {
-//       return response.json();
-//     } else {
-//       throw new Error('Что-то пошло не так на API сервере.');
-//     }
-//   })
-//   .then(response => {
-//     console.debug(response);
-//     // ...
-//   }).catch(error => {
-//     console.error(error);
-//   });
-
-
-
-// function getData() {
-//   return fetch('../db/db.json')
-//     .then(response => {
-//       if (response.ok) {
-//         return response.json();
-//       } else {
-//         throw new Error('Данные не были получены, ошибка: ' + response.status);
-//       }
-//     })
-//     .catch(err => {
-//       console.warn(err);
-//       document.body.innerHTML = '<div style="color: red; font-size: 30px; ">Упс, что-то пошло не так!</div>';
-//     });
-// }
-
-// console.log(getData());
-
+document.querySelector('body').appendChild(component)
 
